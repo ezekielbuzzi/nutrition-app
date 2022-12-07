70d0eb6cc3f1b18f1ddcaddf94974429906413a0
Thili007 <tpunchihewage@gmail.com>
2022-12-07 14:33:21 +0100

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Welcome from './components/welcome-page/Welcome'
-import MainPage from './components/welcome-page/MainPage'
-import RecipeListPage from './components/welcome-page/RecipeListPage'
-import RecipePage from './components/welcome-page/RecipePage'
-import NutritionFactPage from './components/welcome-page/NutritionFactPage'
-import Title from './components/title/Title'
-
+import Welcome from "./components/welcome-page/Welcome";
+import MainPage from "./components/welcome-page/MainPage";
+import RecipeListPage from "./components/welcome-page/RecipeListPage";
+import RecipePage from "./components/welcome-page/RecipePage";
+import NutritionFactPage from "./components/welcome-page/NutritionFactPage";
+import Title from "./components/title/Title";
 
 function App() {
   return (
     <BrowserRouter>
-      <Title/>
+      <Title />
       <Routes>
-        <Route path="/" element={<Welcome/>}/>
-        <Route path="/main" element={<MainPage/>}/>
-        <Route path="/recipe-list" element={<RecipeListPage/>}/>
-        <Route path="/recipe-page" element={<RecipePage/>}/>
-        <Route path="/nutrition-facts" element={<NutritionFactPage/>}/>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/recipe-list" element={<RecipeListPage />} />
+        <Route path="/recipe-page" element={<RecipePage />} />
+        <Route path="/nutrition-facts" element={<NutritionFactPage />} />
       </Routes>
     </BrowserRouter>
   );
