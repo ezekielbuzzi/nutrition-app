300fd1eb6ab7201ed9dd370155b3b629b1be6c5c
itanglishdev <chrishethertonweb@gmail.com>
2022-12-08 08:35:52 +0100

    added MainPage and SelectedList components

--- client/src/components/main-page/MainPage.js
+++ client/src/components/main-page/MainPage.js
@@ -1,7 +1,22 @@
+import SelectedList from "./sub-components/SelectedList";
+
+
+
 function MainPage() {
-    return ( 
-        <div>I am Main Page</div>
-     );
+
+  return (
+    <div className="container-main-page">
+      <div className="container-main-page__left">
+        <SelectedList 
+        // value={something}
+        // id={somethingId}
+        />
+      </div>
+      <div className="container-main-page__right">
+
+      </div>
+    </div>
+  )
 }
 
 export default MainPage;
